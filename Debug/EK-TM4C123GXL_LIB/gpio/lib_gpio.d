2a6e5e645bf02f4ac9a49cb2dd0a0490120eeac5
DRATTAN <zhanyu981@gmail.com>
2023-05-13 12:27:34 +0800

    Signed-off-by: DRATTAN <zhanyu981@gmail.com>

---
+++ Debug/EK-TM4C123GXL_LIB/gpio/lib_gpio.d
@@ -0,0 +1,66 @@
+# FIXED
+
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: ../EK-TM4C123GXL_LIB/gpio/lib_gpio.c
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: ../EK-TM4C123GXL_LIB/gpio/lib_gpio.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: ../EK-TM4C123GXL_LIB/gpio/lib_gpio_define.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+EK-TM4C123GXL_LIB/gpio/lib_gpio.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock_Define.h
+
+../EK-TM4C123GXL_LIB/gpio/lib_gpio.c:
+
+../EK-TM4C123GXL_LIB/gpio/lib_gpio.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+../EK-TM4C123GXL_LIB/gpio/lib_gpio_define.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock_Define.h:
+
---
+++ Debug/blinky.d
@@ -0,0 +1,66 @@
+# FIXED
+
+blinky.obj: ../blinky.c
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+blinky.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+blinky.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock.h
+blinky.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock_Define.h
+blinky.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/gpio/lib_gpio.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h
+blinky.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+blinky.obj: C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/gpio/lib_gpio_define.h
+
+../blinky.c:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/clock/lib_clock_Define.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/gpio/lib_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/Users/RATTAN/Desktop/ccs\ workspace/blinky/EK-TM4C123GXL_LIB/gpio/lib_gpio_define.h:
+
